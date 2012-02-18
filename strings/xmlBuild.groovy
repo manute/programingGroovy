22ec498005fcff12e2e9422a23493b5b826d87f8
Manuel Alonso <manuteali@gmail.com>
2012-02-18 12:01:16 +0100

    build xml with string

---
+++ strings/xmlBuild.groovy
@@ -0,0 +1,19 @@
+langs = ['C++':'Stroustrup','Java':'Gosling','Lisp':'McCarthy']
+
+content = ''
+
+langs.each { language, author ->
+	
+	fragment = """
+	<language name="${language}">
+	 	<author>${author}</author>
+	</language> 
+	"""
+	
+	content+=fragment
+	
+}
+
+xml = "<languages>${content}</languages>"
+
+println xml
